c24e1d382f53fa84243bea0fbdcf831182751b33
Viviane De Lima Francelino <viviane.lfrancelino@sp.senac.br>
2022-12-09 17:16:02 -0300

    atualização login

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,12 @@
 import {Router} from 'express'
+import * as authController from '../controllers/authController'
 
 const router = Router()
 
 router.get('/',(req,res) =>{
     res.send("Teste")
 })
+
+router.get('/login',authController.login)
 
 export default router 
