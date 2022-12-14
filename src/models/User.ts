c0e7fbeb397a074b4f67da16d01fa7ca89a48ff3
VIVIANE DE LIMA FRANCELINO <viviane.lfrancelino@senacsp.edu.br>
2022-12-14 11:48:10 -0300

    atualização login

--- src/models/User.ts
+++ src/models/User.ts
@@ -4,12 +4,8 @@ import { sequelize } from '../instances/mysql'
 export interface UserInstance extends Model {
     id: number,
     nome: string,
-    sobrenome: string,
     email: string,
     senha: string,
-    cpf: string,
-    telefone: string,
-    dataNascimento: string
 }
 export const User = sequelize.define<UserInstance>('User',{
     id: {
@@ -21,10 +17,6 @@ export const User = sequelize.define<UserInstance>('User',{
         type:DataTypes.STRING,
         allowNull:false
     },
-    sobrenome:{
-        type:DataTypes.STRING,
-        allowNull:false
-    },
     email: {
         type:DataTypes.STRING,
         allowNull:false
@@ -32,15 +24,7 @@ export const User = sequelize.define<UserInstance>('User',{
     senha: {
         type:DataTypes.STRING,
         allowNull:false
-    },
-    cpf: {
-        type:DataTypes.STRING
-    },
-    telefone: {
-        type:DataTypes.STRING
-    },
-    dataNascimento: {
-        type:DataTypes.DATE
+
     }
 },{
     tableName:'usuarios',
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,26 +1,14 @@
 import {Router} from 'express'
-import * as authController from '../controllers/authController'
-import * as UserController from '../controllers/userController'
+//import * as authController from '../controllers/authController'
+//import * as UserController from '../controllers/userController'
 
 const router = Router()
 
 router.get('/',(req,res) =>{
-    res.send("Teste")
+    res.render("pages/home")
 })
 
-router.get('/login',authController.login)
 
-// Renderiza a página de cadastro de usuário
-router.get('/cadastro', UserController.renderFormRegister);
-
-// Rota para cadastrar um novo usuário
-router.post('/cadastro', UserController.createNewUser);
-
-// Renderiza a página inicial de login
-router.get('/login', authController.renderLogin);
-
-// Rota para fazer o login do usuário
-router.post('/login', authController.login);
 
 
 
