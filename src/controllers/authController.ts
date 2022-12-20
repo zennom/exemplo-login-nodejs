2fba1dac6999a81267864beaf72188f9829b7664
Viviane De Lima Francelino <viviane.lfrancelino@sp.senac.br>
2022-12-20 10:51:20 -0300

    finalizando views

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express"
 import { User } from "../models/User"
 
 
+
+//rotas de visualização login e register
 export const login = (req:Request, res:Response) =>{
 
     res.render('pages/login')
